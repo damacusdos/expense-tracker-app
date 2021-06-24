94153e64ae295525a950969e179b0360794aea01
Alison Huang <alisonhcw@gmail.com>
2021-06-24 22:34:57 +0800

    add filter year function

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,27 @@
+import ExpenseFilter from "./ExpenseFilter";
 import ExpenseList from "./ExpenseList";
+import { useState } from "react";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2021");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  console.log(props.expenses);
+  const filteredItems = props.expenses.filter(
+    (expense) => expense.date.getFullYear().toString() === filteredYear
+  );
+
   return (
     <div>
-      <ExpenseList expenses={props.expenses} />
+      <ExpenseFilter
+        expenses={props.expenses}
+        selected={filteredYear}
+        onYearChange={filterChangeHandler}
+      />
+      <ExpenseList expenses={filteredItems} />
     </div>
   );
 };
